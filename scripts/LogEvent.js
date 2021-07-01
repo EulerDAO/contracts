8186e6fdb462ab43c8e9b28a4040c42333acc0e3
ds <ds@ds.ds>
2021-07-01 10:41:14 +0800

    make it paralle

--- scripts/LogEvent.js
+++ scripts/LogEvent.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const root_dir = '.cache';
+const timestamp_dir = '.cache/solutions-timestamp';
+const score_dir = '.cache/solutions-score';
 async function main() {
     const EulerDAO = await ethers.getContractFactory("EulerDAO");
     const ed = await EulerDAO.attach('0xC3a65484e3D59689B318fB23c210a079873CFfbB');
@@ -12,70 +15,44 @@ async function main() {
     const ex = await ed.queryFilter(fx, from, to);
     const ey = await ed.queryFilter(fy, from, to);
     const ez = await ed.queryFilter(fz, from, to);
-    const cached = {};
     data.syncblock = to;
+    prepare_dir();
 
+    write_pool = []
     for (const e of ez) {
-        if (cached[e.args.tokenId]) {continue;}
-        cached[e.args.tokenId] = true;
-
-        content = {
-            mint_time: [e.blockNumber, e.transactionIndex],
-            score: await ed.scores(e.args.tokenId),
-            target: await ed.targets(e.args.tokenId),
-        }
-        const dir = '.cache/solutions';
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
-        }
+        content = [e.blockNumber, e.transactionIndex]
         var filename = format(e.args.tokenId);
-        fs.writeFileSync(`${dir}/${filename}.json`, JSON.stringify(content));
+        const res = fs.writeFileSync(`${timestamp_dir}/${filename}.json`, JSON.stringify(content));
+        write_pool.push(res);
     }
+    await Promise.all(write_pool);
 
-    // set(data)
-    const events = [...ex, ...ey];
-    for (const e of events) {
-        const id = e.args.id;
-        var filename = format(e.args.id);
-        const raw = fs.readFileSync(`.cache/solutions/${filename}.json`);
-        const content = JSON.parse(raw);
-        content.score = ethers.BigNumber.from(content.score.hex);
-        content.target = ethers.BigNumber.from(content.target.hex);
 
-        if (!cached[e.args.id]) {
-            cached[e.args.id] = true;
-            content.score = await ed.scores(e.args.id);
-            content.target = await ed.targets(e.args.id);
-            fs.writeFileSync(`.cache/solutions/${filename}.json`, JSON.stringify(content));
-        }
-        {
-            const filename = format(content.target);
-            let solutions = [];
-            try{
-                const raw = fs.readFileSync(`.cache/problems/${filename}.json`);
-                solutions = JSON.parse(raw);
-            } catch {}
-            const index = solutions.indexOf(content.target);
-            if (index > 0) {
-                solutions[index] = content;
-            } else {
-                solutions.push(content);
-            }
-            solutions.sort((x,y)=>{
-                if (x.score > y.score) {return 1}
-                if (x.score < y.score) {return -1}
-                if (x.mint_time[0] > y.score[0]) {return 1}
-                if (x.mint_time[0] < y.mint_time[0]) {return -1}
-                if (x.mint_time[1] > y.mint_time[1]) {return 1}
-                if (x.mint_time[1] > y.mint_time[1]) {return -1}
-                return 1;
-            });
-            const dir = '.cache/problems';
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir);
-            }
-            fs.writeFileSync(`${dir}/${filename}.json`, JSON.stringify(solutions));
-        }
+    score_pool = []
+    for (const e of  [...ex, ...ey]) {
+        score_pool.push(produce_solution_score(ed, e.args.id));
+    }
+    await Promise.all(score_pool);
+}
+
+async function produce_solution_score(ed, id) {
+    var filename = format(id);
+    const raw = fs.readFileSync(`${timestamp_dir}/${filename}.json`);
+    const content = {timestamp: JSON.parse(raw)};
+    content.score = (await ed.scores(id)).toHexString();
+    content.target = (await ed.targets(id)).toHexString();
+    fs.writeFileSync(`${score_dir}/${filename}.json`, JSON.stringify(content));
+}
+
+function prepare_dir() {
+    if (!fs.existsSync(root_dir)) {
+        fs.mkdirSync(root_dir);
+    }
+    if (!fs.existsSync(timestamp_dir)) {
+        fs.mkdirSync(timestamp_dir);
+    }
+    if (!fs.existsSync(score_dir)) {
+        fs.mkdirSync(score_dir);
     }
 }
 
@@ -97,7 +74,7 @@ function set(data) {
 
 function format(bigNumber) {
     bigNumber = bigNumber.toHexString()
-    return '0x000000000000000000000000000000000000000000000000000000000000000000000000000000000000'.substring(0,68 - bigNumber.length) + bigNumber.substring(2)
+    return '0x000000000000000000000000000000000000000000000000000000000000000000000000000000000000'.substring(0, 68 - bigNumber.length) + bigNumber.substring(2)
 }
 
 main()
